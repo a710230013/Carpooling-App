036dd24695cf4f01407dbe196450bb9e2e7fa263
Nicko2102 <nicholas@witzel-milan.com>
2024-11-16 12:05:23 -0500

    start distance calc

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 riders = []
 drivers = []
@@ -13,13 +14,29 @@ def get_time(u):
 
 # gets list of drivers ordered by start time
 with open('backend/driver_data.json') as f:
-    drivers = sorted(json.load(f)["Data"], key=get_time)
+    drivers = sorted(json.load(f)["Data"], key=get_time)[:10]
 
 # gets list of riders ordered by start time
 with open('backend/rider_data.json') as f:
-    riders = sorted(json.load(f)["Data"], key=get_time)
+    riders = sorted(json.load(f)["Data"], key=get_time)[:5]
 
 
 for d in drivers:
+    start_d_loc = d["start_location"].split(",")
+    start_d_x = float(start_d_loc[0])
+    start_d_y = float(start_d_loc[1])
+    end_d_loc = d["destination_location"].split(",")
+    end_d_x = float(end_d_loc[0])
+    end_d_y = float(end_d_loc[1])
+    for r in riders:
+        start_r_loc = r["start_location"].split(",")
+        start_r_x = float(start_r_loc[0])
+        start_r_y = float(start_r_loc[1])
+        end_r_loc = r["destination_location"].split(",")
+        end_r_x = float(end_r_loc[0])
+        end_r_y = float(end_r_loc[1])
+        dist_rider = ((end_r_x - start_r_x) ** 2 + (end_r_y - start_r_y) ** 2)
+        print(start_r_loc, start_r_x, start_r_y)
+        # du_s = r[]
     print(d["name"], d["time_of_travel"])
     
